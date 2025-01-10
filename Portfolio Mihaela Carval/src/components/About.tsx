8754ee4291994f5b9b44e9de53e4f14ebd118b40
Mihaela CARVAL <balan.myhaela@yahoo.com>
2025-01-10 16:52:25 +0100

    feature: first version of my portfolio

--- Portfolio Mihaela Carval/src/components/About.tsx
+++ Portfolio Mihaela Carval/src/components/About.tsx
@@ -1,5 +1,9 @@
 import Section from './Section';
 import Skills from './Skills';
+import { GiBatteries } from "react-icons/gi";
+import { FcIdea } from "react-icons/fc";
+import { TfiCommentsSmiley } from "react-icons/tfi";
+import { FaCalendarCheck } from "react-icons/fa";
 import '../sass/about.scss';
 
 const About: React.FC = () => {
@@ -7,10 +11,37 @@ const About: React.FC = () => {
     <>
       <Section id="about" title="A PROPOS">
         <div className='about_container'>
-        <div className='soft_skills'>Soft skills</div>
-        <div className='speech'>Passionnée et déterminée j'ai a su réinventer mon parcours en passant de la gestion immobilière et comptable au développement web. Habituée à allier vision stratégique et attention aux détails, j'aime créer des interfaces web esthétiques, fonctionnelles et centrées sur l'expérience utilisateur. <br/>Animée par la curiosité et l'envie de relever de nouveaux défis, je suis prête à contribuer avec enthousiasme au monde des technologies web
-        </div>
-        </div>
+          <div className='soft_skills'>
+            <div className="soft_skills_items">
+              <div className="soft_skill">
+                <div className="soft_skill_icon">
+                  <GiBatteries size={32} />
+                </div>
+                <div className="soft_skill_label">Autonome</div>
+              </div>
+              <div className="soft_skill">
+                <div className="soft_skill_icon">
+                  <FcIdea size={32} />
+                </div>
+                <div className="soft_skill_label">Innovante</div>
+              </div>
+              <div className="soft_skill">
+                <div className="soft_skill_icon">
+                  <FaCalendarCheck size={32} />
+                </div>
+                <div className="soft_skill_label">Organisée</div>
+              </div>
+              <div className="soft_skill">
+                <div className="soft_skill_icon">
+                  <TfiCommentsSmiley size={32} />
+                </div>
+                <div className="soft_skill_label">Enthousiaste</div>
+              </div>
+            </div>
+            <div className='speech'>Passionnée et déterminée j'ai a su réinventer mon parcours en passant de la gestion immobilière et comptable au développement web. Habituée à allier vision stratégique et attention aux détails, j'aime créer des interfaces web esthétiques, fonctionnelles et centrées sur l'expérience utilisateur. <br />Animée par la curiosité et l'envie de relever de nouveaux défis, je suis prête à contribuer avec enthousiasme au monde des technologies web
+            </div>
+          </div>
+          </div>
       </Section>
       <Skills />
     </>
